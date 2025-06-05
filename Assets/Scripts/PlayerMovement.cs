ead4aee7be10d7d2714d4d03ade7b00fa940c3e4
karaflito <michaelxara@gmail.com>
2025-06-05 20:42:40 +0100

    Movement added

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float moveSpeed = 5f; // Speed of the player movement
+    private Vector2 moveInput; // Stores the intpu direction from WASD
+
+
+
+
+
+
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovePlayer();
+    }
+
+    public void PlayerInput(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>();
+
+    }
+
+    public void MovePlayer()
+    {
+        Vector2 movement = moveInput.normalized;
+
+        transform.Translate(movement * moveSpeed * Time.deltaTime);
+    }
+
+
+}
